88dc2a3f2993a2a80876e5c48002cfd0e8443663
wufeng321 <48675419+wufeng321@users.noreply.github.com>
2024-04-11 15:39:39 +0800

    Initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 这个文件作为组件的目录
+ * 目的是统一管理对外输出的组件，方便分类
+ */
+/**
+ * 布局组件 todo: 2024/04/11 项目瘦身最下面一行移除SelectLang
+ */
+import Footer from './Footer';
+import { Question } from './RightContent';
+import { AvatarDropdown, AvatarName } from './RightContent/AvatarDropdown';
+export { AvatarDropdown, AvatarName, Footer, Question };
