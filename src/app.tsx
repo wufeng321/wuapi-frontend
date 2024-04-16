361bd94adb77b63e422236c6c69c29a7d1f0c9c3
wufeng321 <48675419+wufeng321@users.noreply.github.com>
2024-04-16 14:56:49 +0800

    修改配置文件名称；优化导入；

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,11 @@
-import { AvatarDropdown, AvatarName, Footer, Question } from '@/components';
-import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
-import { LinkOutlined } from '@ant-design/icons';
-import type { Settings as LayoutSettings } from '@ant-design/pro-components';
-import { SettingDrawer } from '@ant-design/pro-components';
-import type { RunTimeLayoutConfig } from '@umijs/max';
-import { Link, history } from '@umijs/max';
-import defaultSettings from '../config/defaultSettings';
-import { errorConfig } from './requestErrorConfig';
+import {AvatarDropdown, AvatarName, Footer, Question} from '@/components';
+import {LinkOutlined} from '@ant-design/icons';
+import {SettingDrawer} from '@ant-design/pro-components';
+import type {RunTimeLayoutConfig} from '@umijs/max';
+import {history, Link} from '@umijs/max';
+import {requestConfig} from './requestConfig';
 import {getLoginUserUsingGet} from "@/services/wuapi-backend/userController";
-import {tryPaths} from "@umijs/utils";
+
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
@@ -121,6 +118,4 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * 它基于 axios 和 ahooks 的 useRequest 提供了一套统一的网络请求和错误处理方案。
  * @doc https://umijs.org/docs/max/request#配置
  */
-export const request = {
-  ...errorConfig,
-};
+export const request = requestConfig;
